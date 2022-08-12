582ab7c99e78570a2ba30a0927fb04420accb9cf
Diederik Krols <diederik_krols@hotmail.com>
2022-08-12 09:58:16 +0200

    Add project files.

---
+++ XamlBrewer.WinUI3.SqlConnectionSettings.Sample/Controls/SqlConnectionSettingsPanel.xaml.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace XamlBrewer.WinUI3.Controls
+{
+    public sealed partial class SqlConnectionSettingsPanel : UserControl, INotifyPropertyChanged
+    {
+        private bool isBusy = true;
+
+        private string connectionString;
+
+        private List<string> databases = new List<string>();
+
+        private SqlConnectionStringBuilder builder = new();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SqlConnectionSettingsPanel()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is busy.
+        /// </summary>
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current server name.
+        /// </summary>
+        public string Server
+        {
+            get { return builder.DataSource; }
+            set
+            {
+                builder.DataSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        public string UserId
+        {
+            get { return builder.UserID; }
+            set
+            {
+                builder.UserID = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the user password.
+        /// </summary>
+        public string Password
+        {
+            get { return builder.Password; }
+            set
+            {
+                builder.Password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the list of databases.
+        /// </summary>
+        public List<string> Databases
+        {
+            get { return databases; }
+            set
+            {
+                databases = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current database.
+        /// </summary>
+        public string Database
+        {
+            get { return builder.InitialCatalog; }
+            set
+            {
+                builder.InitialCatalog = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current connection string.
+        /// </summary>
+        public string ConnectionString
+        {
+            get { return builder.ConnectionString; }
+            set
+            {
+                connectionString = value;
+                builder.ConnectionString = connectionString;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged()
+        {
+            // Broadcast all properties.
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+
+        private List<string> AuthenticationProtocols => new() {
+            "Windows Authentication",
+            "SQL Server Authentication",
+            "Azure Active Directory - Universal with MFA",
+            "Azure Active Directory - Password",
+            "Azure Active Directory - Integrated"
+        };
+    }
+}
---
+++ XamlBrewer.WinUI3.SqlConnectionSettings.Sample/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace XamlBrewer.WinUI3.SqlConnectionSettings.Sample
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Title = "XAML Brewer WinUI3 SQL Connection Settings Sample";
+            this.InitializeComponent();
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Root.RequestedTheme = Root.ActualTheme == ElementTheme.Light ? ElementTheme.Dark : ElementTheme.Light;
+        }
+    }
+}
